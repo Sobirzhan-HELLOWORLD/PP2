c3900755fc4b6c7b4b48cfbbefc55f753ae53347
Sobirzhan-HELLOWORLD <davlatovsabirzhan@gmail.com>
2019-01-31 11:33:17 +0600

    first commit

---
+++ Week 1/Task3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            int d = int.Parse(Console.ReadLine());/*считываю d  в консоле сразу как интежер*/
+            int[] a = new int[d];   /*создаю массив с размером d*/
+
+            string s = Console.ReadLine();//считываем стринг чтобы он мог прочитать числа
+            string[] arr = s.Split(' ');//идет стринговый массив куда идут числа через пробел 
+
+            for (int i = 0; i < d; i++)
+            {
+              for (int k = 0; k < 2; k++)
+                   {
+                    Console.Write(arr[i]+' ');//выводим в консоль число до 2 значений .потом переходит к следующему
+                }
+
+            }
+            
+        }
+    }
+}
+
