055a6d5ad38a46fad5065ea778644f4039d296af
Sobirzhan-HELLOWORLD <davlatovsabirzhan@gmail.com>
2019-02-01 13:56:08 +0600

    4 task(2)

---
+++ Week 1/Task4/ConsoleApp17/ConsoleApp17/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleApp17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int d = int.Parse(Console.ReadLine());
+
+                        for(int i = 1; i <=d; i++)
+            {
+             for(int j = 1; j <=i; j++)
+                
+                    Console.Write("[*]");
+                    Console.Write("\n");
+                
+            }
+        }
+    }
+}
